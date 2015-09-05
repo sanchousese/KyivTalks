03e06e782f18dfcd65dd824cc9704a7afc6c07fd
sanchousese <saniasutula@gmail.com>
2015-09-05 23:41:31 +0300

    Authorization was written

--- app/models.py
+++ app/models.py
@@ -1,5 +1,8 @@
-from app import db
+from flask import g
+from app import app, db, auth
 from passlib.apps import custom_app_context as pwd_context
+from itsdangerous import (TimedJSONWebSignatureSerializer
+						  as Serializer, BadSignature, SignatureExpired)
 
 
 class User(db.Model):
@@ -14,5 +17,35 @@ class User(db.Model):
 	def verify_password(self, password):
 		return pwd_context.verify(password, self.password_hash)
 
+	def generate_auth_token(self, expiration = 600):
+		s = Serializer(app.config['SECRET_KEY'], expires_in = expiration)
+		return s.dumps({'id': self.id})
+
+	@staticmethod
+	def verify_auth_token(token):
+		s = Serializer(app.config['SECRET_KEY'])
+		try:
+			data = s.loads(token)
+		except SignatureExpired:
+			return None    # valid token, but expired
+		except BadSignature:
+			return None    # invalid token
+		user = User.query.get(data['id'])
+		return user
+
+
 	def __repr__(self):
 		return '<User %r>' % (self.login)
+
+
+@auth.verify_password
+def verify_password(email_or_token, password):
+	# first try to authenticate by token
+	user = User.verify_auth_token(email_or_token)
+	if not user:
+		# try to authenticate with username/password
+		user = User.query.filter_by(email=email_or_token).first()
+		if not user or not user.verify_password(password):
+			return False
+	g.user = user
+	return True
--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
-from app import app
+from app import app, db, auth
+from flask import Flask, abort, request, jsonify, g, url_for
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
@@ -6,17 +8,40 @@ def index():
 	return "Hello, world!"
 
 
-@app.route('/api/users', methods = ['POST'])
+@app.route('/api/new_user', methods = ['POST'])
 def new_user():
-	email = request.json.get('username')
+	login = request.json.get('login')
+	email = request.json.get('email')
 	password = request.json.get('password')
 
+# TODO: add normal error handling
 	if (email is None) or (password is None):
 		abort(400)
 	if User.query.filter_by(email = email).first() is not None:
 		abort(400)
-	user = User(email = email)
+	user = User(email = email, login = login)
 	user.hash_password(password)
 	db.session.add(user)
 	db.session.commit()
-	return jsonify({ 'login': user.login }), 201, {'Location': url_for('get_user', id = user.id, _external=True)}
+	return jsonify({ 'login': user.login, 'email': user.email }), 201, {'Location': url_for('get_user', id = user.id, _external=True)}
+
+
+@app.route('/api/users/<int:id>')
+def get_user(id):
+	user = User.query.get(id)
+	if not user:
+		abort(400)
+	return jsonify({'login': user.login})
+
+
+@app.route('/api/token')
+@auth.login_required
+def get_auth_token():
+	token = g.user.generate_auth_token(600)
+	return jsonify({'token': token.decode('ascii'), 'duration': 600})
+
+
+@app.route('/api/resource')
+@auth.login_required
+def get_resource():
+	return jsonify({'data': 'Hello, %s!' % g.user.login})
